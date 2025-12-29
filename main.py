e7279b738fa0c41f3e128feb0baac45c35dc4090
suyogrk <13267148+suyogrk@users.noreply.github.com>
2025-12-30 00:44:33 +0545

    feat: system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import argparse
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -25,6 +26,7 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash',
         contents=messages,
+         config=types.GenerateContentConfig(system_instruction=system_prompt),
         )
     
     usage_metadata = response.usage_metadata
